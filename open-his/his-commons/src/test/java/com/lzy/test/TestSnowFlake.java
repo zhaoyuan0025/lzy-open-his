77c0f00f1c289485ab6f5e1192a2ed8babc11618
xiaoyuan <xiaoyuan@qq.com>
2020-11-18 11:09:36 +0800

    测试雪花算法的工具类

---
+++ open-his/his-commons/src/test/java/com/lzy/test/TestSnowFlake.java
@@ -0,0 +1,16 @@
+package com.lzy.test;
+
+import com.lzy.utils.IdGeneratorSnowflake;
+
+/**
+ * @program: open-his
+ * @description: 测试类
+ * @author: lzy
+ * @create: 2020-11-18 11:01
+ **/
+public class TestSnowFlake {
+
+    public static void main(String[] args) {
+
+    }
+}
