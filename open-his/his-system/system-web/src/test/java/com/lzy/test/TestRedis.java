e3fd6853135385232630b01f5e2252c1e55e6e3c
zhaoyuan <liushao_zz@163.com>
2021-02-26 11:13:44 +0800

    添加mybatis的依赖

--- open-his/his-system/system-web/src/test/java/com/lzy/test/TestRedis.java
+++ open-his/his-system/system-web/src/test/java/com/lzy/test/TestRedis.java
@@ -1,9 +1,11 @@
 package com.lzy.test;
 
+import cn.hutool.core.lang.UUID;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.lzy.utils.IdWorker;
 import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
@@ -42,5 +44,27 @@ public class TestRedis {
         DecodedJWT jwt = build.verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDkyNjI4NTQsInVzZXJuYW1lIjoiMTIzNDU2NDUifQ.ZpXPj50cJk1h4sIhrkHt0MkyBY6z3ieRizoidOqNofk");
         System.out.println(jwt.getClaim("username").asString());
 
+    }
+
+    /**
+     * 测试雪花算法生成的唯一编号
+     */
+//    @Autowired
+//    private IdWorker idWorker;
+    @Test
+    public void testIdWorker(){
+        IdWorker idWorker = new IdWorker();
+        for (int i=0;i<100;i++){
+//            Long s = idWorker.nextId();
+//            System.out.println("雪花算法编号的:"+s);
+
+//            long time = System.nanoTime();
+//            System.out.println("系统时间生成的："+time);
+
+            //UUID 生成的编号含义没有这么明确，并且
+            String s = UUID.randomUUID().toString().replace("-","");
+            System.out.println("UUID生成的唯一编号"+s);
+
+        }
     }
 }
